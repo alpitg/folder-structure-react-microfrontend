6f15f2333ca1582d4af94c7c4432c5b0af64d1b3
alpitg <alpitgajbhiye@gmail.com>
2025-08-31 13:51:51 +0530

    minor

--- micro-posterica/src/components/ui/toast/toast.tsx
+++ micro-posterica/src/components/ui/toast/toast.tsx
@@ -41,32 +41,34 @@ const ToastApp: React.FC<ToastAppProps> = ({
   }, [show, delay, onClose]);
 
   return (
-    <div
-      className="toast-container position-fixed top-0 end-0 p-3"
-      style={{ zIndex: 1080 }}
-    >
+    show && (
       <div
-        ref={toastRef}
-        className={`toast bg-${variant} ${
-          isLightBackground ? "text-dark" : "text-white"
-        } border-0`}
-        role="alert"
-        aria-live="assertive"
-        aria-atomic="true"
+        className="toast-container position-fixed top-0 end-0 p-3"
+        style={{ zIndex: 1080 }}
       >
-        <div className="d-flex">
-          <div className="toast-body">{message}</div>
-          <button
-            type="button"
-            className={`btn-close ${
-              !isLightBackground ? "btn-close-white" : ""
-            } me-2 m-auto`}
-            data-bs-dismiss="toast"
-            aria-label="Close"
-          ></button>
+        <div
+          ref={toastRef}
+          className={`toast bg-${variant} ${
+            isLightBackground ? "text-dark" : "text-white"
+          } border-0`}
+          role="alert"
+          aria-live="assertive"
+          aria-atomic="true"
+        >
+          <div className="d-flex">
+            <div className="toast-body">{message}</div>
+            <button
+              type="button"
+              className={`btn-close ${
+                !isLightBackground ? "btn-close-white" : ""
+              } me-2 m-auto`}
+              data-bs-dismiss="toast"
+              aria-label="Close"
+            ></button>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
