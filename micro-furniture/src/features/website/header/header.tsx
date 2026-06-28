8d5c69663d9af773096a532ac30669ee5583b100
alpitg <alpitgajbhiye@gmail.com>
2026-06-29 01:24:45 +0530

    cart

--- micro-furniture/src/features/website/header/header.tsx
+++ micro-furniture/src/features/website/header/header.tsx
@@ -30,7 +30,7 @@ const HeaderApp = () => {
 
         <div className="collapse navbar-collapse" id="navbarsFurni">
           <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-            <li className="nav-item active">
+            <li className="nav-item">
               <NavLink className="nav-link" to={ROUTE_URL.WEBSITE.BASE}>
                 Home
               </NavLink>
@@ -56,7 +56,8 @@ const HeaderApp = () => {
           <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
             <li>
               <NavLink className="nav-link" to={ROUTE_URL.WEBSITE.CART}>
-                <img src="/static/media/img/cart.svg" />
+                <i className="bi bi-cart3 fs-xl me-1"></i> 
+                Cart
               </NavLink>
             </li>
           </ul>
